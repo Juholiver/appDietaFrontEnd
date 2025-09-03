de2f9916475e6f1f693fce491bdbb662099dc1cb
Juholiver <juholiver3@gmail.com>
2025-09-03 11:22:11 -0300

    Criando Header em componente

--- app/step/index.tsx
+++ app/step/index.tsx
@@ -1,13 +1,14 @@
-import { Text, View } from 'react-native';
+import { View } from 'react-native';
 import { Header } from '../../components/header';
 
 export default function Step() {
   return (
 
     <View>
-      <Header />
-      <Text>TESTE </Text>
-    
+      <Header
+        step= "Passo 1"
+        title="Vamos começar"
+      />
     </View>
 
   );
--- components/header/index.tsx
+++ components/header/index.tsx
@@ -1,9 +1,65 @@
-import { Text, View } from 'react-native';
+import { router } from 'expo-router';
+import {
+  Platform,
+  Pressable,
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+import Feather from 'react-native-vector-icons/Feather';
+import { colors } from '../../constants/colors';
 
-export function Header() {
+interface HeaderProps {
+  step: string;
+  title: string;
+}
+
+export function Header({step,title}: HeaderProps) {
   return (
-    <View >
-      <Text>Header aqui</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.content}>
+          <View style={styles.row}>
+            <Pressable onPress={() => router.replace('/')}>
+              <Feather name="arrow-left" size={24} color="#000" />	
+            </Pressable>
+            <Text style={styles.text}> {step} <Feather name="loader" size={16} color="#000" /></Text>
+          </View>
+
+          <Text style={styles.tittle}>
+            {title}
+          </Text>
+      </View>
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: colors.white,
+    borderBottomRightRadius:14,
+    borderBottomLeftRadius:14,
+    marginBottom:14,
+    paddingTop:Platform.OS === 'android' ? StatusBar.currentHeight! + 34 : 34,
+  },
+  content: {
+    paddingLeft: 16,
+    paddingRight: 16,
+    paddingBottom: 34,
+  },
+  row: {
+    flexDirection: 'row',
+    gap: 8,
+    alignItems: 'center',
+  },
+  text:{
+    fontSize:18,
+    color:colors.black,
+  },
+  tittle:{
+    fontSize:30,
+    color:colors.background,
+    fontWeight:'bold',
+  }
+});
